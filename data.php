f45cd0c3e8ad344752abc5f9fa033d91c9e344ae
Kenneth Brenes <kenneth.brenes@kbrenes-mbp.local>
2014-05-23 18:28:52 -0600

    Adding data

---
+++ data.php
@@ -0,0 +1,11 @@
+<?php
+//species, sequences, genes
+include('main.php');
+include('template/header.php');
+
+//performe the search action
+
+include('template/table.php');
+include('template/footer2.php');
+
+?>	
